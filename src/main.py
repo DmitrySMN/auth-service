5d3993c4fa15291fb099fd029ae34c3224b3b69c
Dmitry Semenov <ddmtry064@gmail.com>
2025-04-24 09:33:44 +0300

    init commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def main():
+    return {"route": "root"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
