770bf063837edd958c8526570f737e2ae4e97bb3
Dmitry Semenov <ddmtry064@gmail.com>
2025-04-24 10:23:13 +0300

    fix: pre settings fix

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
-from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from db.session import *
 from db.models import Base
-
-app = FastAPI()
+from contextlib import asynccontextmanager
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as connection:
-        await connection.run_sync(Base.metadata.create_all())
+        await connection.run_sync(Base.metadata.create_all)
     yield
 
+app = FastAPI(lifespan=lifespan)
+
+
 @app.get("/")
-def main():
+def index():
     return {"route": "root"}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app")
+    uvicorn.run("main:app", reload=True)
